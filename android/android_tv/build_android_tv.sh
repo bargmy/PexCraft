97d5d11ca82344ccda9f7d2db8925b40cf493494
bargmy <saam12647@gmail.com>
2026-06-26 17:32:12 +0330

    1.2.5 aye

--- android/android_tv/build_android_tv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "${BASH_SOURCE[0]}")"
-bash ./prepare_sdl2.sh
-if [ -x ./gradlew ]; then ./gradlew assembleRelease; else gradle assembleRelease; fi
--- android/main_wii.c
+++
@@ -1,2 +0,0 @@
-#define PEX_PLATFORM_WII 1
-#include "src/main_wii.c"
