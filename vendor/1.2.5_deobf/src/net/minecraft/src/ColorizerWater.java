757a36306311213f496d0850145a622177792185
bargmy <saam12647@gmail.com>
2026-06-29 21:42:04 +0330

    yea

--- vendor/1.2.5_deobf/src/net/minecraft/src/ColorizerWater.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-public class ColorizerWater {
-	private static int[] waterBuffer = new int[65536];
-
-	public static void setWaterBiomeColorizer(int[] var0) {
-		waterBuffer = var0;
-	}
-}
--- vendor/1.2.5_deobf/src/net/minecraft/src/StatTypeSimple.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.src;
-
-final class StatTypeSimple implements IStatType {
-	public String format(int var1) {
-		return StatBase.getNumberFormat().format((long)var1);
-	}
-}
