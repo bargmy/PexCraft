50f1c2779346409b0432e81ac96985dd9cbc6954
bargmy <saam12647@gmail.com>
2026-07-06 21:35:31 +0330

    downloader x2

--- src/platform/sdl2_app.c
+++ src/platform/sdl2_app.c
@@ -303,6 +303,291 @@ static void main_loop(void) {
     SDL_StopTextInput();
 }
 
+
+static int g_sdl2_native_resource_prompt_handled = 0;
+
+static void sdl2_apply_release_resource_selection(int textures, int audio_mask) {
+    g_opts.download_classic_textures = textures ? 1 : 0;
+    g_opts.download_classic_sounds = (audio_mask & CLASSIC_AUDIO_ALL) ? 1 : 0;
+    g_opts.classic_audio_mask = audio_mask & CLASSIC_AUDIO_ALL;
+    if (!g_opts.download_classic_textures && !g_opts.download_classic_sounds) {
+        g_opts.ignore_classic_resources_warning = 1;
+        g_opts.ignore_classic_sounds_warning = 1;
+    } else {
+        g_opts.ignore_classic_resources_warning = 0;
+        g_opts.ignore_classic_sounds_warning = 0;
+    }
+    save_options();
+}
+
+static int sdl2_release_textures_installed(void) {
+    return pack_is_installed() && !pack_missing_required_textures();
+}
+
+static int sdl2_release_audio_installed_mask(void) {
+    int mask = 0;
+    if (classic_sounds_installed_mask(CLASSIC_AUDIO_MOBS)) mask |= CLASSIC_AUDIO_MOBS;
+    if (classic_sounds_installed_mask(CLASSIC_AUDIO_WORLD_UI)) mask |= CLASSIC_AUDIO_WORLD_UI;
+    if (classic_sounds_installed_mask(CLASSIC_AUDIO_RECORDS)) mask |= CLASSIC_AUDIO_RECORDS;
+    if (classic_sounds_installed_mask(CLASSIC_AUDIO_MENU_MUSIC)) mask |= CLASSIC_AUDIO_MENU_MUSIC;
+    if (classic_sounds_installed_mask(CLASSIC_AUDIO_GAME_MUSIC)) mask |= CLASSIC_AUDIO_GAME_MUSIC;
+    return mask & CLASSIC_AUDIO_ALL;
+}
+
+static int sdl2_release_audio_missing_mask(void) {
+    return CLASSIC_AUDIO_ALL & ~sdl2_release_audio_installed_mask();
+}
+
+static void sdl2_release_check_line(char *out, size_t cap, const char *name, int selected, int installed) {
+    snprintf(out, cap, "[%c] %-24s %s", installed ? '-' : (selected ? 'X' : ' '), name, installed ? "(installed - locked)" : "");
+}
+
+static void sdl2_release_resource_selection_text(char *out, size_t cap, int textures, int audio_mask) {
+    char size_line[MAX_LABEL];
+    char line_tex[96], line_mob[96], line_ui[96], line_disc[96], line_menu[96], line_game[96];
+    int textures_installed = sdl2_release_textures_installed();
+    int installed_audio = sdl2_release_audio_installed_mask();
+    int missing_audio = CLASSIC_AUDIO_ALL & ~installed_audio;
+    int old_tex = g_opts.download_classic_textures;
+    int old_snd = g_opts.download_classic_sounds;
+    int old_mask = g_opts.classic_audio_mask;
+
+    textures = textures_installed ? 0 : (textures ? 1 : 0);
+    audio_mask &= missing_audio;
+
+    g_opts.download_classic_textures = textures ? 1 : 0;
+    g_opts.download_classic_sounds = (audio_mask & CLASSIC_AUDIO_ALL) ? 1 : 0;
+    g_opts.classic_audio_mask = audio_mask & CLASSIC_AUDIO_ALL;
+    pack_install_start_size_fetch();
+    format_download_size(size_line, sizeof(size_line));
+    g_opts.download_classic_textures = old_tex;
+    g_opts.download_classic_sounds = old_snd;
+    g_opts.classic_audio_mask = old_mask;
+
+    sdl2_release_check_line(line_tex, sizeof(line_tex), "Textures", textures, textures_installed);
+    sdl2_release_check_line(line_mob, sizeof(line_mob), "Mob sounds", audio_mask & CLASSIC_AUDIO_MOBS, installed_audio & CLASSIC_AUDIO_MOBS);
+    sdl2_release_check_line(line_ui, sizeof(line_ui), "UI / block / world sounds", audio_mask & CLASSIC_AUDIO_WORLD_UI, installed_audio & CLASSIC_AUDIO_WORLD_UI);
+    sdl2_release_check_line(line_disc, sizeof(line_disc), "Music discs", audio_mask & CLASSIC_AUDIO_RECORDS, installed_audio & CLASSIC_AUDIO_RECORDS);
+    sdl2_release_check_line(line_menu, sizeof(line_menu), "Main menu music", audio_mask & CLASSIC_AUDIO_MENU_MUSIC, installed_audio & CLASSIC_AUDIO_MENU_MUSIC);
+    sdl2_release_check_line(line_game, sizeof(line_game), "In-game music", audio_mask & CLASSIC_AUDIO_GAME_MUSIC, installed_audio & CLASSIC_AUDIO_GAME_MUSIC);
+
+    snprintf(out, cap,
+        "PexCraft Release Resources\n\n"
+        "This is a native launcher popup, not an in-game menu.\n"
+        "Nothing downloads until you press Download selected.\n\n"
+        "Already downloaded items are shown as installed/locked and cannot be toggled.\n\n"
+        "Current checklist:\n"
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n\n"
+        "%s\n\n"
+        "Use the category buttons to toggle only missing items, Select All Missing to toggle all missing items,\n"
+        "or Play without selected to start without downloading.",
+        line_tex, line_mob, line_ui, line_disc, line_menu, line_game, size_line);
+}
+
+static int sdl2_native_release_resources_run_download(void) {
+    SDL_Window *win = NULL;
+    SDL_Renderer *ren = NULL;
+    HANDLE th = NULL;
+    int cancelled = 0;
+    LONG state;
+
+    pack_install_reset_cancel();
+    if (g_classic_install_thread) { CloseHandle(g_classic_install_thread); g_classic_install_thread = NULL; }
+    g_classic_install_error[0] = 0;
+    pack_install_set_state(CLASSIC_INSTALL_DOWNLOADING, 0, "Preparing Release resources...");
+    th = CreateThread(NULL, 0, pack_install_worker, NULL, 0, NULL);
+    if (!th) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, APP_TITLE " - Resources", "Could not start the resource download thread.", NULL);
+        pack_install_fail("Could not start installer thread");
+        return 0;
+    }
+
+    win = SDL_CreateWindow(APP_TITLE " - Downloading Release Resources", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 560, 180, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (win) ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_SOFTWARE);
+    if (!win || !ren) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_WARNING, APP_TITLE " - Resources", "Download is running. This SDL build could not open the progress window.", NULL);
+    }
+
+    for (;;) {
+        SDL_Event e;
+        state = InterlockedCompareExchange(&g_classic_install_state, 0, 0);
+        if (state == CLASSIC_INSTALL_DONE || state == CLASSIC_INSTALL_ERROR) break;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                cancelled = 1;
+                pack_install_request_cancel();
+            } else if (e.type == SDL_WINDOWEVENT && win && e.window.windowID == SDL_GetWindowID(win)) {
+                if (e.window.event == SDL_WINDOWEVENT_CLOSE) {
+                    cancelled = 1;
+                    pack_install_request_cancel();
+                }
+            } else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) {
+                cancelled = 1;
+                pack_install_request_cancel();
+            }
+        }
+        if (win) {
+            char title[MAX_LABEL * 2];
+            int p = (int)InterlockedCompareExchange(&g_classic_install_progress, 0, 0);
+            if (p < 0) p = 0;
+            if (p > 100) p = 100;
+            snprintf(title, sizeof(title), "%s - %d%% - %s%s", APP_TITLE, p, g_classic_install_status[0] ? g_classic_install_status : "Downloading...", cancelled ? " - canceling" : "");
+            SDL_SetWindowTitle(win, title);
+            if (ren) {
+                int w = 560, h = 180;
+                SDL_GetWindowSize(win, &w, &h);
+                SDL_SetRenderDrawColor(ren, 28, 28, 32, 255);
+                SDL_RenderClear(ren);
+                SDL_Rect outline = {40, h / 2 - 12, w - 80, 24};
+                SDL_Rect fill = {42, h / 2 - 10, (w - 84) * p / 100, 20};
+                SDL_SetRenderDrawColor(ren, 120, 120, 120, 255);
+                SDL_RenderDrawRect(ren, &outline);
+                SDL_SetRenderDrawColor(ren, cancelled ? 160 : 80, cancelled ? 80 : 170, cancelled ? 80 : 90, 255);
+                SDL_RenderFillRect(ren, &fill);
+                SDL_RenderPresent(ren);
+            }
+        }
+        SDL_Delay(33);
+    }
+
+    WaitForSingleObject(th, INFINITE);
+    CloseHandle(th);
+    if (ren) SDL_DestroyRenderer(ren);
+    if (win) SDL_DestroyWindow(win);
+
+    state = InterlockedCompareExchange(&g_classic_install_state, 0, 0);
+    if (state == CLASSIC_INSTALL_DONE) {
+        InterlockedExchange(&g_classic_install_state, CLASSIC_INSTALL_IDLE);
+        if (g_opts.download_classic_textures && pack_is_installed()) {
+            snprintf(g_opts.skin, sizeof(g_opts.skin), "%s", CLASSIC_PACK_NAME);
+            save_options();
+        }
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, APP_TITLE " - Resources", "Selected Release resources were downloaded.", NULL);
+        return 1;
+    }
+
+    InterlockedExchange(&g_classic_install_state, CLASSIC_INSTALL_IDLE);
+    if (cancelled || !strcmp(g_classic_install_error, "Canceled")) {
+        g_opts.download_classic_textures = 0;
+        g_opts.download_classic_sounds = 0;
+        g_opts.classic_audio_mask = 0;
+        g_opts.ignore_classic_resources_warning = 1;
+        g_opts.ignore_classic_sounds_warning = 1;
+        save_options();
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, APP_TITLE " - Resources", "Resource download canceled. The game will continue without the selected resources.", NULL);
+    } else {
+        char msg[MAX_LABEL * 3];
+        snprintf(msg, sizeof(msg), "Could not install Release resources.\n\n%s\n\nThe game will continue without silently retrying in-game.", g_classic_install_error[0] ? g_classic_install_error : "Unknown error");
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, APP_TITLE " - Resources", msg, NULL);
+    }
+    return 0;
+}
+
+static void sdl2_native_release_resources_prompt(void) {
+    int textures_installed = sdl2_release_textures_installed();
+    int missing_audio = sdl2_release_audio_missing_mask();
+    int textures = (g_opts.download_classic_textures && !textures_installed) ? 1 : 0;
+    int audio_mask = g_opts.download_classic_sounds ? (g_opts.classic_audio_mask & missing_audio) : 0;
+    int running = 1;
+
+    g_sdl2_native_resource_prompt_handled = 1;
+    if (!textures && !audio_mask) {
+        textures = textures_installed ? 0 : 1;
+        audio_mask = missing_audio;
+    }
+
+    while (running) {
+        char text[2048];
+        int buttonid = -1;
+        SDL_MessageBoxButtonData buttons[9];
+        int nb = 0;
+        int all_missing_selected;
+        int any_missing;
+        textures_installed = sdl2_release_textures_installed();
+        missing_audio = sdl2_release_audio_missing_mask();
+        textures = textures_installed ? 0 : (textures ? 1 : 0);
+        audio_mask &= missing_audio;
+        any_missing = (!textures_installed) || missing_audio;
+        all_missing_selected = (textures_installed || textures) && ((audio_mask & missing_audio) == missing_audio);
+
+        buttons[nb++] = (SDL_MessageBoxButtonData){ SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 100, "Download selected" };
+        buttons[nb++] = (SDL_MessageBoxButtonData){ SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 101, "Play without selected" };
+        if (any_missing) buttons[nb++] = (SDL_MessageBoxButtonData){ 0, 102, "Select All Missing" };
+        if (!textures_installed) buttons[nb++] = (SDL_MessageBoxButtonData){ 0, 103, "Textures" };
+        if (missing_audio & CLASSIC_AUDIO_MOBS) buttons[nb++] = (SDL_MessageBoxButtonData){ 0, 104, "Mob sounds" };
+        if (missing_audio & CLASSIC_AUDIO_WORLD_UI) buttons[nb++] = (SDL_MessageBoxButtonData){ 0, 105, "UI/Block" };
+        if (missing_audio & CLASSIC_AUDIO_RECORDS) buttons[nb++] = (SDL_MessageBoxButtonData){ 0, 106, "Discs" };
+        if (missing_audio & CLASSIC_AUDIO_MENU_MUSIC) buttons[nb++] = (SDL_MessageBoxButtonData){ 0, 107, "Menu music" };
+        if (missing_audio & CLASSIC_AUDIO_GAME_MUSIC) buttons[nb++] = (SDL_MessageBoxButtonData){ 0, 108, "In-game music" };
+
+        {
+            SDL_MessageBoxColorScheme colors = {{
+                { 245, 245, 245 },
+                { 32, 32, 36 },
+                { 64, 64, 70 },
+                { 255, 255, 255 },
+                { 40, 40, 46 }
+            }};
+            SDL_MessageBoxData data;
+            memset(&data, 0, sizeof(data));
+            sdl2_release_resource_selection_text(text, sizeof(text), textures, audio_mask);
+            data.flags = SDL_MESSAGEBOX_INFORMATION;
+            data.window = NULL;
+            data.title = APP_TITLE " - Release Resources";
+            data.message = text;
+            data.numbuttons = nb;
+            data.buttons = buttons;
+            data.colorScheme = &colors;
+            if (SDL_ShowMessageBox(&data, &buttonid) != 0) buttonid = 101;
+        }
+
+        switch (buttonid) {
+            case 100:
+                textures = textures_installed ? 0 : textures;
+                audio_mask &= missing_audio;
+                if (!textures && !audio_mask) {
+                    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_WARNING, APP_TITLE " - Resources", "Nothing missing is selected. Choose missing resources or press Play without selected.", NULL);
+                    break;
+                }
+                sdl2_apply_release_resource_selection(textures, audio_mask);
+                sdl2_native_release_resources_run_download();
+                running = 0;
+                break;
+            case 101:
+            case -1:
+                g_opts.download_classic_textures = 0;
+                g_opts.download_classic_sounds = 0;
+                g_opts.classic_audio_mask = 0;
+                g_opts.ignore_classic_resources_warning = 1;
+                g_opts.ignore_classic_sounds_warning = 1;
+                save_options();
+                running = 0;
+                break;
+            case 102:
+                if (all_missing_selected) { textures = 0; audio_mask = 0; }
+                else { textures = textures_installed ? 0 : 1; audio_mask = missing_audio; }
+                break;
+            case 103: if (!textures_installed) textures = !textures; break;
+            case 104: if (missing_audio & CLASSIC_AUDIO_MOBS) audio_mask ^= CLASSIC_AUDIO_MOBS; break;
+            case 105: if (missing_audio & CLASSIC_AUDIO_WORLD_UI) audio_mask ^= CLASSIC_AUDIO_WORLD_UI; break;
+            case 106: if (missing_audio & CLASSIC_AUDIO_RECORDS) audio_mask ^= CLASSIC_AUDIO_RECORDS; break;
+            case 107: if (missing_audio & CLASSIC_AUDIO_MENU_MUSIC) audio_mask ^= CLASSIC_AUDIO_MENU_MUSIC; break;
+            case 108: if (missing_audio & CLASSIC_AUDIO_GAME_MUSIC) audio_mask ^= CLASSIC_AUDIO_GAME_MUSIC; break;
+            default:
+                running = 0;
+                break;
+        }
+        if (running) {
+            InterlockedExchange(&g_classic_download_size_state, CLASSIC_SIZE_UNKNOWN);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     for (int i = 1; i < argc; ++i) {
         if (argv[i] && (!strcmp(argv[i], "--loggy") || !strcmp(argv[i], "--logger") || !strcmp(argv[i], "--diagnostics"))) g_loggy_enabled = 1;
@@ -324,9 +609,10 @@ int main(int argc, char **argv) {
     init_dirs();
     srand((unsigned int)time(NULL));
     load_options();
-    /* Never download Release resources silently on SDL2.  The main window is
-       created first, then SCREEN_CLASSIC_PACK_DOWNLOAD_PROMPT asks the player
-       before any network download starts. */
+    /* Native resource prompt must happen before the game window/GL UI exists.
+       It does not use Minecraft textures, fonts, or in-game screens, and it
+       does not download anything until the player presses Download selected. */
+    if (should_show_pack_download_prompt()) sdl2_native_release_resources_prompt();
     char sound_dep_notice[MAX_LABEL * 3];
     if (!pex_sound_record_dependency_report(sound_dep_notice, sizeof(sound_dep_notice))) {
         char msg[MAX_LABEL * 4];
@@ -362,8 +648,8 @@ int main(int argc, char **argv) {
         loggy_init();
         if (g_glrc) SDL_GL_MakeCurrent(g_hwnd, g_glrc);
     }
-    if (should_show_pack_download_prompt()) set_screen(SCREEN_CLASSIC_PACK_DOWNLOAD_PROMPT);
-    else if (!strcmp(g_opts.skin, CLASSIC_PACK_NAME) && classic_resources_need_update()) set_screen(SCREEN_CLASSIC_PACK_WARNING);
+    if (!g_sdl2_native_resource_prompt_handled && should_show_pack_download_prompt()) set_screen(SCREEN_CLASSIC_PACK_DOWNLOAD_PROMPT);
+    else if (!g_sdl2_native_resource_prompt_handled && !strcmp(g_opts.skin, CLASSIC_PACK_NAME) && classic_resources_need_update()) set_screen(SCREEN_CLASSIC_PACK_WARNING);
     else set_screen(pex_startup_screen());
     InitializeCriticalSection(&g_save_cs);
     main_loop();
